dc9ae5dd206b3981fead75d9c0e02fe126bdf2a2
iam1513 <126111227+iam1513@users.noreply.github.com>
2024-01-03 14:31:56 +0530

    Time: 92 ms (50.15%), Space: 23.6 MB (50.15%) - LeetHub

--- 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.cpp
+++ 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.cpp
@@ -1,27 +1,31 @@
 class Solution {
 public:
-    int numberOfBeams(vector<string>& bank) {
-        
+    int numberOfBeams(std::vector<std::string>& bank) {
         int r = bank.size();
         int c = bank[0].size();
+        int cnt = 0;
+        std::vector<int> ans;
 
-        int prev = 0;
-        int ans = 0;
-        for(int i = 0; i < r; i++){
-            int cnt = 0;
-            for(int j = 0; j < c; j++){
-                
-                if(bank[i][j] == '1'){
+        for (int i = 0; i < r; i++) {
+            cnt = 0;
+            for (int j = 0; j < c; j++) {
+                if (bank[i][j] == '1') {
                     cnt++;
-                }           
+                }
             }
-            if(cnt!=0){
-                ans += (prev * cnt);
-                prev = cnt;
-            }
-            
+            ans.push_back(cnt);
         }
+
+        int sol = 0;
+        int prev = 0;
         
-        return ans;
+        for(int i = 0; i < r; i++){
+            if(ans[i] != 0){
+                sol += ans[i]*prev;
+                prev = ans[i];
+            }
+        }
+
+        return sol;
     }
 };
