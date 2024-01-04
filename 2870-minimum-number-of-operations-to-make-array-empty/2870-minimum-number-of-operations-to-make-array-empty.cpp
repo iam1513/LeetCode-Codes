42a7d4f3649c73a8a5e35d43b6be0ee556dc4b0f
iam1513 <126111227+iam1513@users.noreply.github.com>
2024-01-04 10:16:35 +0530

    Time: 120 ms (61.88%), Space: 85.2 MB (46.04%) - LeetHub

--- 2870-minimum-number-of-operations-to-make-array-empty/2870-minimum-number-of-operations-to-make-array-empty.cpp
+++ 2870-minimum-number-of-operations-to-make-array-empty/2870-minimum-number-of-operations-to-make-array-empty.cpp
@@ -4,42 +4,77 @@ public:
         
         int n = nums.size();
         
-        int maxe = INT_MIN;
+        unordered_map<int,int> m;
         
-        for(int i = 0; i < n; i++){
-            if(nums[i]>maxe){
-                maxe = nums[i];
-            }
-        }
-        
-                
-        vector<int> ans(maxe+1,0);
-        
-        for(int i = 0; i < n; i++){
-            ans[nums[i]]++;
+        for( auto ele: nums){
+            m[ele]++;
         }
         
         int sum = 0;
         
-        for(int i = 0; i < ans.size(); i++){
-            if(ans[i] == 1){
+        for( auto ele: m){
+            if(ele.second == 1){
                 return -1;
             }
-        }
-        
-        for(int i = 0; i < ans.size(); i++){
-            if(ans[i] % 3 == 0 && ans[i]!=0){
-                sum += ans[i]/3;
-                ans[i] = 0;
+            
+            if(ele.second % 3 == 0 && ele.second!=0){
+                sum += ele.second/3;
+                ele.second = 0;
             }
             
-            else if((ans[i] % 3 == 2 && ans[i]>1)|| (ans[i] % 3 == 1 && ans[i]>1)){
-                sum += ans[i]/3 + 1;
-                ans[i] = 0;
+            else if((ele.second % 3 == 2 && ele.second>1) || (ele.second % 3 == 1 && ele.second>1)){
+                sum += ele.second/3 + 1;
+                ele.second = 0;
             }
-            
         }
         
         return sum;
     }
 };
+
+
+// class Solution {
+// public:
+//     int minOperations(vector<int>& nums) {
+        
+//         int n = nums.size();
+        
+//         int maxe = INT_MIN;
+        
+//         for(int i = 0; i < n; i++){
+//             if(nums[i]>maxe){
+//                 maxe = nums[i];
+//             }
+//         }
+        
+                
+//         vector<int> ans(maxe+1,0);
+        
+//         for(int i = 0; i < n; i++){
+//             ans[nums[i]]++;
+//         }
+        
+//         int sum = 0;
+        
+//         for(int i = 0; i < ans.size(); i++){
+//             if(ans[i] == 1){
+//                 return -1;
+//             }
+//         }
+        
+//         for(int i = 0; i < ans.size(); i++){
+//             if(ans[i] % 3 == 0 && ans[i]!=0){
+//                 sum += ans[i]/3;
+//                 ans[i] = 0;
+//             }
+            
+//             else if((ans[i] % 3 == 2 && ans[i]>1)|| (ans[i] % 3 == 1 && ans[i]>1)){
+//                 sum += ans[i]/3 + 1;
+//                 ans[i] = 0;
+//             }
+            
+//         }
+        
+//         return sum;
+//     }
+// };
