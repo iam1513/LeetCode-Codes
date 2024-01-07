8b459cca310a9f1b1671f42a8940398bd793eb06
iam1513 <126111227+iam1513@users.noreply.github.com>
2024-01-07 16:25:08 +0530

    Time: 4 ms (25.48%), Space: 8.3 MB (19.10%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,18 +1,41 @@
+// APPROACH 2 : DOWN-UP --> TAB
+
 class Solution {
 public:
-    
-    vector<int> dp;
-    
-    int helper(int i,vector<int>& arr){
-        if(i >= arr.size()){
-            return 0;
-        }
-        if(dp[i]!=-1) return dp[i];
-        return dp[i] = max(arr[i]+helper(i+2,arr),helper(i+1,arr));
-    }
-    
     int rob(vector<int>& nums) {
-        dp.resize(nums.size()+10,-1);
-        return helper(0,nums);
+        int n = nums.size();
+        if(n==1) return nums[n-1];
+        vector<int> dp(n+10,-1);
+        dp[n-1] = nums[n-1];
+        dp[n-2] = max(nums[n-1],nums[n-2]);
+        
+        for(int i = n-3; i >=0; i--){
+            dp[i] = max(dp[i+2]+nums[i],dp[i+1]);
+        }
+        
+        return dp[0];
     }
 };
+
+
+
+// APPROACH 1 : TOP-DOWN --> MEMO
+
+// class Solution {
+// public:
+    
+//     vector<int> dp;
+    
+//     int helper(int i,vector<int>& arr){
+//         if(i >= arr.size()){
+//             return 0;
+//         }
+//         if(dp[i]!=-1) return dp[i];
+//         return dp[i] = max(arr[i]+helper(i+2,arr),helper(i+1,arr));
+//     }
+    
+//     int rob(vector<int>& nums) {
+//         dp.resize(nums.size()+10,-1);
+//         return helper(0,nums);
+//     }
+// };
