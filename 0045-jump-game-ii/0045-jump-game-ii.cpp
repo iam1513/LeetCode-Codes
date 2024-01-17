83a8dc3e299e3d446a7a8d3dac14ec6fed91ae1c
iam1513 <126111227+iam1513@users.noreply.github.com>
2024-01-17 16:31:11 +0530

    Time: 8 ms (91.03%), Space: 16.8 MB (98.19%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -2,14 +2,19 @@ class Solution {
 public:
     int jump(vector<int>& nums) {
         int jumps = 0;
-        int currentMaxReach = 0;
-        int nextMaxReach = 0;
+        int currMax = 0;
+        int nextMax = 0;
 
         for (int i = 0; i < nums.size() - 1; ++i) {
-            nextMaxReach = max(nextMaxReach, i + nums[i]);
+            
+            // Calculating nextMax 
+            nextMax = max(nextMax, i + nums[i]);
 
-            if (i == currentMaxReach) {
-                currentMaxReach = nextMaxReach;
+            
+            // will update when we will reach the index where we can at max reach
+            // when we will reach that index, then we will get kow the next index we can reavh as already stored in nextMax 
+            if (i == currMax) {
+                currMax = nextMax;
                 jumps++;
             }
         }
