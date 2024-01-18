b59c6e18261d4c52147893e8321f47bb62725ea1
iam1513 <126111227+iam1513@users.noreply.github.com>
2024-01-18 13:57:32 +0530

    Time: 2 ms (45.07%), Space: 6.4 MB (71.72%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -24,26 +24,53 @@
 // };
 
 
-// APPROACH 2
+// APPROACH 2 
+// class Solution {
+// public:
+
+//     vector<int> dp;
+
+//     int f(int n){
+//         // Base case
+//         if(n == 0) return 1;
+//         // If it is at last step and then it takes 2 steps
+//         if(n<0) return 0;
+//         if(dp[n] != -1) return dp[n];
+
+//         return dp[n] = f(n-1) + f(n-2);
+//     }
+
+//     int climbStairs(int n) {
+//         dp.clear();
+//         dp.resize(50,-1);
+//         return f(n);
+
+//     }
+// };
+
+
+// APPROACH 3 : BOTTOM UP
+
 class Solution {
 public:
-
+    
     vector<int> dp;
-
-    int f(int n){
-        // Base case
-        if(n == 0) return 1;
-        // If it is at last step and then it takes 2 steps
-        if(n<0) return 0;
-        if(dp[n] != -1) return dp[n];
-
-        return dp[n] = f(n-1) + f(n-2);
-    }
-
+    
     int climbStairs(int n) {
         dp.clear();
         dp.resize(50,-1);
-        return f(n);
-
+        
+        // Corner case
+        if(n == 1) return 1;
+        
+        dp[n-1] = 1; // Present on the top stair
+        dp[n-2] = 2; // Present on the second last stair
+        
+        for(int i = n-3 ; i>=0 ; i--){
+            dp[i] = dp[i+1] + dp[i+2];
+        }
+        
+        return dp[0];
+        
     }
 };
