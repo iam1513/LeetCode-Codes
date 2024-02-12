506a84def83af3cf4d96c46d99551c0c4fe4a076
iam1513 <126111227+iam1513@users.noreply.github.com>
2024-02-12 14:31:07 +0530

    Time: 6 ms (8.25%), Space: 10.7 MB (5.35%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -4,36 +4,45 @@ public:
     vector<int> dp;
     
     int string_to_int(string s){
-        if(s.size() == 1) return s[0] - '0'; // String to int conversion
-        return ((s[0] - '0')*10 + (s[1] - '0')); // returning 2 digit int
-    }
-    
-    int helper(int idx, string s){
-        
-        if(s[idx] == '0') return 0;
-        if(idx>=s.size()) return 0;
-        
-        if(idx == s.size()-1){
-            if(s[idx] =='0') return 0;
-            else return 1;
+        if(s.size() == 1){
+            return s[0] - '0';
         }
         
-        if(idx == s.size() - 2){
-            bool can_form_2_dig = string_to_int(s.substr(idx,2)) <= 26;
-            return can_form_2_dig + helper(idx+1,s);
+        else{
+            return (s[0] - '0')*10 + (s[1]- '0');
+        }
+    }
+    
+    int ways(string str, int idx){
+        
+        if(str[idx] == '0') return 0;
+        
+        if(idx>=str.size()) return 0;
+        
+        if(idx == str.size() - 1){
+            if(str[idx]=='0') return 0;
+            else{
+                return 1;
+            }
+        }
+        
+        if(idx == str.size() - 2){
+            bool can_form_2_dig = string_to_int(str.substr(idx,2)) <= 26;
+            return can_form_2_dig +ways(str,idx+1);
         }
         
         if(dp[idx] != -1) return dp[idx];
         
-        bool can_form_2_dig = string_to_int(s.substr(idx,2)) <= 26; // Passing substring of length 2 and checking if its smaller than 26 or not
+        bool can_form_2_digits = string_to_int(str.substr(idx,2)) <= 26;
         
-        return dp[idx] = helper(idx+1,s) + ((can_form_2_dig) ? helper(idx+2,s) : 0);
+        return dp[idx]  = ways(str,idx+1) + (can_form_2_digits ? ways(str,idx+2) : 0);
         
     }
     
     int numDecodings(string s) {
-        dp.clear(); // To remove garbage values if any
+        dp.clear();
         dp.resize(105,-1);
-        return helper(0,s);
+        
+        return ways(s,0);
     }
 };
