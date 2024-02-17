53b0cda759adf0eca7c30ad2d0a87be9f4449f13
iam1513 <126111227+iam1513@users.noreply.github.com>
2024-02-17 22:11:08 +0530

    Time: 0 ms (100.00%), Space: 8.1 MB (23.63%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        if(n==0)
+        return 1;
+        
+        if(n==1)
+        return x;
+        
+        if (n < 0)
+          return 1 /(x*myPow(x, -(n+1)));
+        double res = myPow(x,n/2);
+        if(n%2==0){
+            return res*res;
+        }
+        else{
+            return x*res*res;
+        }
+    }
+};
