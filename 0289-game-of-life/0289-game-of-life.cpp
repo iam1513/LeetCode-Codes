a76b3490a670cce1e17437c78577ffb011d6d28f
iam1513 <126111227+iam1513@users.noreply.github.com>
2024-02-20 09:36:48 +0530

    Time: 4 ms (20.77%), Space: 8.3 MB (34.17%) - LeetHub

---
+++ 0289-game-of-life/0289-game-of-life.cpp
@@ -0,0 +1,78 @@
+class Solution {
+public:
+    
+    int f(vector<vector<int>>& board, int i, int j){
+        int r = board.size();
+        int c = board[0].size();
+        int cnt = 0;
+        
+        if(i-1>=0 && j-1>=0 && board[i-1][j-1] == 1){
+            cnt++;
+        }
+        
+        if(i-1>=0 && j+1<c && board[i-1][j+1] == 1){
+            cnt++;
+        }
+        
+        if(i+1<r && j+1<c && board[i+1][j+1] == 1){
+            cnt++;
+        }
+        
+        if(i+1<r && j-1>=0 && board[i+1][j-1] == 1){
+            cnt++;
+        }
+        
+        if(i-1>=0 &&  board[i-1][j] == 1){
+            cnt++;
+        }
+        
+        if(j-1>=0 &&  board[i][j-1] == 1){
+            cnt++;
+        }
+        
+        if(i+1<r &&  board[i+1][j] == 1){
+            cnt++;
+        }
+        
+        if(j+1<c &&  board[i][j+1] == 1){
+            cnt++;
+        }
+        
+        return cnt;
+    }
+
+    void gameOfLife(vector<vector<int>>& board) {
+
+        int rows = board.size();
+        int cols = board[0].size();
+        
+        vector<vector<int>> a(rows,vector<int> (cols,0));
+        for(int i =0 ; i < rows; i++){
+            for(int j = 0; j < cols; j++){
+                int cnt = f(board, i, j);
+                
+                if(board[i][j] == 1 && cnt < 2){
+                    a[i][j] = 0;
+                }
+                
+                else if(board[i][j] == 1 && (cnt > 3)){
+                    a[i][j] = 0;
+                }
+                
+                else if(board[i][j] == 0 && cnt==3){
+                    a[i][j] = 1;
+                }
+                
+                else if(board[i][j] == 1 && (cnt==2 || cnt==3)){
+                    a[i][j] = 1;
+                }
+            }
+        }
+        
+        for(int i = 0; i < rows; i++){
+            for(int j = 0; j < cols;j++){
+                board[i][j] = a[i][j];
+            }
+        }
+    }
+};
