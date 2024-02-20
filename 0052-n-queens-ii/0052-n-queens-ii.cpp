a16c89b9fd5b740bb5ba03212926202da119d46a
iam1513 <126111227+iam1513@users.noreply.github.com>
2024-02-20 17:21:58 +0530

    Time: 3 ms (72.65%), Space: 7.4 MB (45.12%) - LeetHub

---
+++ 0052-n-queens-ii/0052-n-queens-ii.cpp
@@ -0,0 +1,65 @@
+
+
+class Solution {
+public:
+    
+    void updateV(int v){
+        v = v+1;
+    }
+    
+    bool canWePlace(vector<vector<string>> &ans, int n,int r, int c){
+        
+        for(int i = r-1,j=c-1; i>=0 && j>=0; i--,j-- ){
+            if(ans[i][j] == "Q"){
+                return false;
+            }
+        }
+        
+        for(int i = r-1,j=c+1; i>=0 && j<n; i--,j++ ){
+            if(ans[i][j] == "Q"){
+                return false;
+            }
+        }
+        
+        for(int j=c-1; j>=0;j-- ){
+            if(ans[r][j] == "Q"){
+                return false;
+            }
+        }
+        
+        for(int i=r-1; i>=0;i-- ){
+            if(ans[i][c] == "Q"){
+                return false;
+            }
+        }
+        
+        return true;
+    }
+    
+    int f(vector<vector<string>> &ans, int n,int r){
+        
+        if(r==n){
+           return 1;
+        }
+        
+        int cnt = 0;
+        for(int j = 0; j < n; j++){
+            if(canWePlace(ans,n,r,j)){
+                ans[r][j] = "Q";
+                cnt += f(ans,n,r+1);
+                ans[r][j] = ".";
+            }
+        }     
+        
+        return cnt;
+        
+    }
+    
+    int totalNQueens(int n) {
+        vector<vector<string>> ans(n, vector<string> (n,"."));
+        
+        
+        return f(ans,n,0);
+        
+    }
+};
