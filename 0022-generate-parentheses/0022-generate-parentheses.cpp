1693b030d5da9fb78c5ad2f7b975b1cc20eceafe
iam1513 <126111227+iam1513@users.noreply.github.com>
2024-03-14 20:04:30 +0530

    Time: 12 ms (6.31%), Space: 20.1 MB (6.62%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,11 +1,9 @@
+// USING STACK
+
 class Solution {
 public:
     
     void f(int i, int j, vector<string> &ans, int n,stack<char> & st){
-        
-        if(j>i){
-            return;
-        }
         
         if(i==n && j == n){
             
@@ -28,7 +26,7 @@ public:
         f(i + 1, j, ans, n, st);
         st.pop();}
         
-        if(j<n){st.push(')');
+        if(j<i){st.push(')');
         f(i, j + 1, ans, n, st);
         st.pop();}
         
@@ -44,3 +42,52 @@ public:
         
     }
 };
+
+
+// USING STRING
+// class Solution {
+// public:
+    
+//     void f(int i, int j, vector<string> &ans, int n,stack<char> & st){
+        
+//         if(j>i){
+//             return;
+//         }
+        
+//         if(i==n && j == n){
+            
+//             string str = "";
+//             stack<char> s = st;
+//             while(!s.empty()){
+//                 str+=s.top();
+//                 s.pop();
+//             }
+            
+//             reverse(str.begin(),str.end());
+            
+//             ans.push_back(str);
+//             return ;
+//         }
+        
+        
+        
+//        if(i<n){ st.push('(');
+//         f(i + 1, j, ans, n, st);
+//         st.pop();}
+        
+//         if(j<n){st.push(')');
+//         f(i, j + 1, ans, n, st);
+//         st.pop();}
+        
+        
+//     }
+    
+//     vector<string> generateParenthesis(int n) {
+//         vector<string> ans;
+//         stack<char> st;
+//         f(0,0,ans,n,st);
+        
+//         return ans;
+        
+//     }
+// };
