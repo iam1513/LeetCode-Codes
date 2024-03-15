1710937f7141784ef769839e6249633af30bdd42
iam1513 <126111227+iam1513@users.noreply.github.com>
2024-03-15 20:30:27 +0530

    Time: 264 ms (20.53%), Space: 138.1 MB (17.67%) - LeetHub

--- 0077-combinations/0077-combinations.cpp
+++ 0077-combinations/0077-combinations.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     
-    void combinations(int idx , int n, int cnt, int k, vector<int> v, vector<vector<int>>& ans, vector<int> nums){
+    void combinations(int idx , int n, int cnt, int k, vector<int> v, vector<vector<int>>& ans){
         if(cnt == k){
             ans.push_back(v);
             return;
         }
         
-        for( int i = idx; i < n; i++){
-            v.push_back(nums[i]);
-            combinations(i+1,n,cnt+1,k,v,ans,nums);
+        for( int i = idx; i <= n; i++){
+            v.push_back(i);
+            combinations(i+1,n,cnt+1,k,v,ans);
             v.pop_back();
         }
     }
@@ -18,12 +18,40 @@ public:
         vector<vector<int>> ans;
         vector<int> v;
         int cnt = 0;
-        vector<int> nums;
-        for(int i = 1; i <=n; i++){
-            nums.push_back(i);
-        }
-        combinations(0,n,cnt,k,v,ans,nums);
+        combinations(1,n,cnt,k,v,ans);
         
         return ans;
     }
 };
+
+// USING VECTOR TO STORE NUMBERS
+
+// class Solution {
+// public:
+    
+//     void combinations(int idx , int n, int cnt, int k, vector<int> v, vector<vector<int>>& ans, vector<int> nums){
+//         if(cnt == k){
+//             ans.push_back(v);
+//             return;
+//         }
+        
+//         for( int i = idx; i < n; i++){
+//             v.push_back(nums[i]);
+//             combinations(i+1,n,cnt+1,k,v,ans,nums);
+//             v.pop_back();
+//         }
+//     }
+    
+//     vector<vector<int>> combine(int n, int k) {
+//         vector<vector<int>> ans;
+//         vector<int> v;
+//         int cnt = 0;
+//         vector<int> nums;
+//         for(int i = 1; i <=n; i++){
+//             nums.push_back(i);
+//         }
+//         combinations(0,n,cnt,k,v,ans,nums);
+        
+//         return ans;
+//     }
+// };
