864c408e2a626c27796ac0e4b6543e9a62c6e880
iam1513 <126111227+iam1513@users.noreply.github.com>
2024-05-27 10:22:46 +0530

    Time: 8 ms (74.39%), Space: 16.6 MB (14.38%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -15,24 +15,111 @@ public:
             }
         }
         
-        int n = rows.size(); int m = cols.size();
         
-        
-        for(int i = 0; i < n; i++){
-            int k = rows[i];
-            for(int j = 0; j < c; j++){
-                matrix[k][j] = 0;
+        for(int i = 0 ; i < r; i++){
+            for(int j = 0; j < cols.size(); j++){
+                int temp = cols[j];
+                matrix[i][temp] = 0;
             }
         }
         
-        for(int i = 0; i < r; i++){
-            for(int j = 0; j < m; j++){
-                int k = cols[j];
-                matrix[i][k] = 0;
+        for(int i = 0 ; i < rows.size(); i++){
+            int temp = rows[i];
+            for(int j = 0; j < c; j++){
+                
+                matrix[temp][j] = 0;
             }
         }
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+// public:
+//     void setZeroes(vector<vector<int>>& matrix) {
+//         int r = matrix.size();
+//         int c = matrix[0].size();
+//         vector<int> rows;
+//         vector<int> cols;
+        
+//         for(int i = 0; i < r; i++){
+//             for(int j = 0; j < c; j++){
+//                 if(matrix[i][j] == 0){
+//                     rows.push_back(i);
+//                     cols.push_back(j);
+//                 }
+//             }
+//         }
+        
+//         int n = rows.size(); int m = cols.size();
+        
+        
+//         for(int i = 0; i < n; i++){
+//             int k = rows[i];
+//             for(int j = 0; j < c; j++){
+//                 matrix[k][j] = 0;
+//             }
+//         }
+        
+//         for(int i = 0; i < r; i++){
+//             for(int j = 0; j < m; j++){
+//                 int k = cols[j];
+//                 matrix[i][k] = 0;
+//             }
+//         }
+//     }
+// };
 
 
 // // Won't work for negative inputs
