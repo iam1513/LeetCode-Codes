d191b838a2cbac7a3179fcd2cf91e0092cf9ae2c
iam1513 <126111227+iam1513@users.noreply.github.com>
2024-06-09 07:05:34 +0530

    Time: 15 ms (77.44%), Space: 26.8 MB (47.19%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,16 +1,43 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        unordered_map<int,int> m;
-        int n = nums.size();
-        for(auto ele:nums){
-            m[ele]++;
-        }
-        for(auto ele:m){
-            if(ele.second > n/2){
-                return ele.first;
+        // USING MOORE'S VOTING ALGORITHM
+        int ele,cnt = 0;
+        
+        for(int i = 0; i < nums.size(); i++){
+            if(cnt == 0){
+                ele = nums[i];
+                cnt++;
+            }
+            
+            else if(ele == nums[i]){
+                cnt++;
+            }
+            
+            else{
+                cnt--;
             }
         }
-        return -1;
+        
+        return ele;
     }
 };
+
+
+// APPROACH 2 : EXTRA SPACE 
+// class Solution {
+// public:
+//     int majorityElement(vector<int>& nums) {
+//         unordered_map<int,int> m;
+//         int n = nums.size();
+//         for(auto ele:nums){
+//             m[ele]++;
+//         }
+//         for(auto ele:m){
+//             if(ele.second > n/2){
+//                 return ele.first;
+//             }
+//         }
+//         return -1;
+//     }
+// };
