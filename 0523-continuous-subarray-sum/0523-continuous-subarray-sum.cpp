d01f3661ebf8fd12039c573e53dc2d65a523f6e6
iam1513 <126111227+iam1513@users.noreply.github.com>
2024-06-09 21:43:31 +0530

    Time: 176 ms (66.72%), Space: 134.6 MB (59.63%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
@@ -1,20 +1,26 @@
 class Solution {
 public:
     bool checkSubarraySum(vector<int>& nums, int k) {
-        unordered_map<int,int> mp;
-        mp[0] = -1;
-        int ps = 0;
-        
-        for(int i = 0; i < nums.size(); i++){
-            ps =(nums[i]+ps)%k;
+        unordered_map<int, int> mp;
+        mp[0] = 0; // Initialize with 0 to handle subarray starting from index 0
+        int ps = 0; // Prefix sum
+
+        for(int i = 0; i < nums.size(); i++) {
+            ps += nums[i];
+            int psm = ps % k; // Prefix sum modulo k
             
-            if(mp.find(ps)!=mp.end()){
-                if(i - mp[ps]>1){
+            // Handle negative modulo result
+            if (psm < 0) {
+                psm += k;
+            }
+            
+            if (mp.find(psm) != mp.end()) {
+                // If the subarray length is greater than 1
+                if (i + 1 - mp[psm] > 1) {
                     return true;
                 }
-            }
-            else{
-                mp[ps] = i;
+            } else {
+                mp[psm] = i + 1;
             }
         }
         
