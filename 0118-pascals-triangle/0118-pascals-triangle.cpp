d406076e5c88cb225a8452449d0694ce49016e52
iam1513 <126111227+iam1513@users.noreply.github.com>
2024-06-11 19:51:37 +0530

    Time: 4 ms (33.60%), Space: 7.9 MB (46.91%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -5,11 +5,10 @@ public:
         
         for(int i = 0; i < numRows; i++){
             ans[i].resize(i+1);
-            for(int j = 0; j <= i; j++){
+            for(int j = 0 ; j <=i; j++){
                 if(j == 0 || j == i){
                     ans[i][j] = 1;
                 }
-                
                 else{
                     ans[i][j] = ans[i-1][j-1] + ans[i-1][j];
                 }
@@ -17,8 +16,6 @@ public:
         }
         
         return ans;
-        
-        
     }
 };
 
@@ -26,76 +23,3 @@ public:
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// class Solution {
-// public:
-//     vector<vector<int>> generate(int numRows) {
-//         int n = numRows;
-//         vector<vector<int>> pascal(n);//vector with n rows
-//         for(int i=0;i<n;i++){
-//             pascal[i].resize(i+1);//first row have 1 column, row  2 have two column and so on
-//             for(int j=0;j<i+1;j++){
-//                 if(j == 0 || j== i){
-//                     pascal[i][j] = 1;//first and last place will always have 1 
-//                 }
-//                 else{
-//                     pascal[i][j] = pascal[i-1][j] + pascal[i-1][j-1];
-//                 }
-//             }
-//         }
-//             return pascal;
-//         }
-// };
