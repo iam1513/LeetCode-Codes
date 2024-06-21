508f250f0a8e3ea09e7c09d41861c3d71744e4f7
iam1513 <126111227+iam1513@users.noreply.github.com>
2024-06-21 11:07:50 +0530

    Time: 23 ms (13.89%), Space: 17.5 MB (44.14%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        
+        vector<int> graph[numCourses];
+        vector<bool> visited(numCourses,false);
+        vector<int> ans;
+        vector<int> inDeg(numCourses,0);
+        
+        for(auto nei : prerequisites){
+            graph[nei[1]].push_back(nei[0]);
+            inDeg[nei[0]]++;
+        }
+        
+        queue<int> q;
+        
+        for(int i = 0; i < numCourses; i++){
+            if(inDeg[i] == 0) q.push(i);
+            // ans.push_back(i);
+        }
+        
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            visited[node] = true;
+             ans.push_back(node);
+            for(int i = 0; i < graph[node].size(); i++){
+                inDeg[graph[node][i]]--;
+                if(inDeg[graph[node][i]] == 0 && visited[graph[node][i]] == false){
+                    q.push(graph[node][i]);
+                    
+                    visited[graph[node][i]] = true;
+                    
+                }
+                
+            }
+        }
+        
+        for(int i = 0; i < numCourses; i++){
+            if(inDeg[i] > 0){
+                
+                return {};
+            }
+        }
+        
+        return ans;
+    }
+};
